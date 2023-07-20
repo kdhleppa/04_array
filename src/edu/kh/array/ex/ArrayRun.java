e5387005ec5b6e8e40d99b0dad472a541ba3741c
kdhleppa <newkdh8624@gmail.com>
2023-07-20 12:34:45 +0900

    1st

---
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,17 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		
+		ArrayExample1 arrayEx1 = new ArrayExample1();
+		
+		arrayEx1.ex8();
+		
+		
+		
+		
+
+	}
+
+}
